8612e8c7c56c321e6e61c364650bd095feb03ee5
bright <lious68@163.com>
2018-11-07 15:42:55 +0800

    2

--- config.simple.py
+++
@@ -1,7 +0,0 @@
-#-*- encoding: utf-8 -*-
-#配置公私钥"""
-public_key  = "ueHWtl+1PQvivQefCBGwRXWfCXlejji9FQxH/ULJ29jB3EP1u/+3OA=="
-private_key = "b24ff301702f9dbeb548752e662b5729e4ad086b"
-project_id = "org-0pjydt" # 项目ID 请在Dashbord 上获取
-
-base_url    = "https://api.ucloud.cn"
